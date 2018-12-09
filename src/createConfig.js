d56b6143fb80b1aed3ca795ecca9968088acf06a
Gitlab-CI <dev@kopaxgroup.com>
2018-12-09 15:08:15 +0700

    fix(createConfig): create config

--- src/createConfig.js
+++ src/createConfig.js
@@ -288,6 +288,49 @@ export function createConfig(config = {}, options = {}) {
             },
           }],
         },
+        {
+          test: /\.(jpe?g|png|gif)$/,
+          use: [
+            {
+              loader: 'url-loader',
+              options: {
+                // Inline files smaller than 10 kB
+                limit: 10 * 1024,
+              },
+            },
+            {
+              loader: 'image-webpack-loader',
+              options: {
+                mozjpeg: {
+                  enabled: false,
+                  // NOTE: mozjpeg is disabled as it causes errors in some Linux environments
+                  // Try enabling it in your environment by switching the config to:
+                  // enabled: true,
+                  // progressive: true,
+                },
+                gifsicle: {
+                  interlaced: false,
+                },
+                optipng: {
+                  optimizationLevel: 7,
+                },
+                pngquant: {
+                  quality: '65-90',
+                  speed: 4,
+                },
+              },
+            },
+          ],
+        },
+        {
+          test: /\.(mp4|webm)$/,
+          use: {
+            loader: 'url-loader',
+            options: {
+              limit: 10000,
+            },
+          },
+        },
       ],
     },
   }, finalWebpackConfig || {}, userWebpackConfig || {});
